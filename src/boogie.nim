0a5bd43c465e58e652ece0bb28ac11d5761ca695
George Lemon <georgelemon@protonmail.com>
2026-05-06 18:54:54 +0300

    update main file
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/boogie.nim
+++ src/boogie.nim
@@ -3,5 +3,11 @@
 # (c) 2025 George Lemon | LGPLv3 License
 #          Made by Humans from OpenPeeps
 #          https://github.com/openpeeps/boogie
+when not defined(builddocs):
+  {.error: "This module is not meant to be imported directly. Import a specific store module instead".}
+else:
+  import ./boogie/wal
+  export wal
 
-{.error: "This module is not meant to be imported directly. Import a specific store module instead".}
+  import ./boogie/stores/[columnar, rdbms, kvstore, graphstore, vectorstore]
+  export columnar, rdbms, kvstore, graphstore, vectorstore
